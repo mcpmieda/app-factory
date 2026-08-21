44fb7fb7d495c0a16e24efdbf319554c410c2f27
mcpmieda <mcpmieda@gmail.com>
2026-08-21 15:16:11 -0300

    release: align starter config to V1.0

--- starters/web-admin/template/src/config/project.ts
+++ starters/web-admin/template/src/config/project.ts
@@ -12,5 +12,5 @@ export const projectConfig = projectConfigSchema.parse({
   description:
     "Uma base administrativa enxuta para implementar a primeira fatia funcional.",
   profile: "web-admin",
-  factoryBaseline: "v1.0.0-rc.1",
+  factoryBaseline: "v1.0.0",
 });
